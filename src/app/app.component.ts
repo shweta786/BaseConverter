727450253185f6c2e879cb3ee4520afb8156af2b
shweta786 <shwetasrivastava1994@gmail.com>
2017-11-09 16:28:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  form;
+  ngOnInit(){
+    this.form = new FormGroup ({
+      decimal : new FormControl(""),
+      binary: new FormControl(""),
+      octal: new FormControl(""),
+      hexa: new FormControl("")
+    });
+  }
+
+  d=0;
+  b=0;
+  o=0;
+  h=0;
+
+  decimalChanged= function(oldValue, newValue){
+    this.d = this.d + 1;
+    if(this.d == 1) {
+      if(newValue != "") {
+        this.form.patchValue({binary: parseInt(newValue,10).toString(2)});
+        this.form.patchValue({octal: parseInt(newValue,10).toString(8)});
+        this.form.patchValue({hexa: parseInt(newValue,10).toString(16).toUpperCase()});
+      } else {
+        this.form.patchValue({binary: " "});
+        this.form.patchValue({octal: " "});
+        this.form.patchValue({hexa: " "});
+      }
+      this.d = 0;
+    }    
+  }
+
+  binaryChanged= function(oldValue, newValue){
+    this.b = this.b + 1;
+    if(this.b == 1) {
+      if(newValue != "") {
+        this.form.patchValue({decimal: parseInt(newValue,2).toString(10)});
+        this.form.patchValue({octal: parseInt(newValue,2).toString(8)});
+        this.form.patchValue({hexa: parseInt(newValue,2).toString(16).toUpperCase()});
+      } else {
+        this.form.patchValue({decimal: " "});
+        this.form.patchValue({octal: " "});
+        this.form.patchValue({hexa: " "});
+      }
+      this.b = 0;
+    }    
+  }
+  
+  octalChanged= function(oldValue, newValue){
+    this.o = this.o + 1;
+    if(this.o == 1) {
+      if(newValue != "") {
+        this.form.patchValue({decimal: parseInt(newValue,8).toString(10)});
+        this.form.patchValue({binary: parseInt(newValue,8).toString(2)});
+        this.form.patchValue({hexa: parseInt(newValue,8).toString(16).toUpperCase()});
+      } else {
+        this.form.patchValue({binary: " "});
+        this.form.patchValue({decimal: " "});
+        this.form.patchValue({hexa: " "});
+      }
+      this.o = 0;
+    }
+  }
+
+  hexaChanged= function(oldValue, newValue){
+    this.h = this.h + 1;
+    if(this.h == 1) {
+      if(newValue != "") {
+        this.form.patchValue({decimal: parseInt(newValue,16).toString(10)});
+        this.form.patchValue({binary: parseInt(newValue,16).toString(2)});
+        this.form.patchValue({octal: parseInt(newValue,16).toString(8)});
+      } else {
+        this.form.patchValue({binary: " "});
+        this.form.patchValue({decimal: " "});
+        this.form.patchValue({octal: " "});
+      }
+      this.h = 0;
+    }    
+  }
+
 }
